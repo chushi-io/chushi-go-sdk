1c45ff115672e94b987d4651afc1c77b046970d1
Robert Wittman <robkwittman@gmail.com>
2024-10-02 20:24:55 -0400

    Add plans update endpoint

--- plans.go
+++ plans.go
@@ -1,7 +1,10 @@
 package chushi
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/hashicorp/go-tfe"
+	"github.com/hashicorp/jsonapi"
 )
 
 type Plans struct {
@@ -9,8 +12,30 @@ type Plans struct {
 }
 
 type UpdatePlanRequest struct {
+	ID     string         `jsonapi:"primary,plans"`
+	Status tfe.PlanStatus `jsonapi:"attr,status"`
 }
 
 func (p *Plans) Update(planId string, req *UpdatePlanRequest) (*tfe.Plan, error) {
-	return nil, nil
+	response := new(tfe.Plan)
+	resp, err := p.sdk.Client.
+		R().
+		SetBody(map[string]interface{}{
+			"data": map[string]interface{}{
+				"id":   planId,
+				"type": "plans",
+				"attributes": map[string]interface{}{
+					"status": req.Status,
+				},
+			},
+		}).
+		Patch(fmt.Sprintf("/api/v2/plans/%s", planId))
+	if err != nil {
+		return nil, err
+	}
+
+	if err := jsonapi.UnmarshalPayload(bytes.NewReader(resp.Body()), response); err != nil {
+		return nil, err
+	}
+	return response, nil
 }
